a8c290cecc57694674a6c71fdefecc41c8f5050e
Ridley Campbell <78474749+Ridley-C@users.noreply.github.com>
2021-05-03 09:14:12 +0200

    Fixed server issues!

--- main.js
+++ main.js
@@ -1,56 +1,89 @@
-import 'ol/ol.css';
-import Map from 'ol/Map';
-import TileLayer from 'ol/layer/Tile';
-import TileWMS from 'ol/source/TileWMS';
-import View from 'ol/View';
+/////////////////////////////////// VARIABLES //////////////////////////////
 
-var wmsSource = new TileWMS({
-  url: 'https://ahocevar.com/geoserver/wms',
-  params: {'LAYERS': 'ne:ne', 'TILED': true},
-  serverType: 'geoserver',
-  crossOrigin: 'anonymous',
+//display mouse position over map. 
+var mousePositionControl = new ol.control.MousePosition({
+  className: 'custom-mouse-position', // place the mouse position in a special div instead of on the map
+  target: document.getElementById('mouse-position'),
+  undefinedHTML: 'Mouse Coords : ',
 });
 
-var wmsLayer = new TileLayer({
-  source: wmsSource,
-});
+// base map layer
+var raster =new ol.layer.Tile({ 
+    source: new ol.source.OSM(),
+  });
 
-var view = new View({
-  center: [0, 0],
-  zoom: 1,
-});
+var map_view = new ol.View({
+    center : [343434, 5744750],
+    zoom : 16,
+    //center: ol.proj.fromLonLat([3.0913,45.78063]), // [place delille]
+    //zoom: 16,
+  });
 
-var map = new Map({
-  layers: [wmsLayer],
+
+//////////////////////////////////// ADD LAYERS //////////////////////////////////////////////
+
+
+//POINTS : geoserver link
+var imgsrc_points = new ol.source.TileWMS({
+      url: 'http://localhost:8080/geoserver/AM_workspace/wms', // link to db [serveur carto ign]
+      params: {'LAYERS': 'AM_workspace:planet_osm_point', 'TILED': false},
+      serverType: 'geoserver',
+      //crossOrigin: 'anonymous', // THIS is supposed to help with CORS issues, but it stops me even loading the layers
+    });
+// Points Tile layer
+var osm_points = new ol.layer.Tile({ 
+    extent: [341520, 5742997, 345218, 5746411], // clermont ! check layer bounds in geoserver for this.
+    source: imgsrc_points
+  });
+
+
+//LINES : geoserver link
+var imgsrc_lines = new ol.source.TileWMS({
+      url: 'http://localhost:8080/geoserver/AM_workspace/wms', 
+      params: {'LAYERS': 'AM_workspace:planet_osm_line', 'TILED': false},
+      serverType: 'geoserver',
+      //crossOrigin: 'anonymous', // same here, this craetes a CORS issue on layer load
+    });
+// Lines Tile Layer
+var osm_lines = new ol.layer.Tile({ 
+    extent: [341520, 5742997, 345218, 5746411],
+    source: imgsrc_lines
+  });
+
+
+
+// ************
+// add the layers to map
+var map = new ol.Map({
+  controls: ol.control.defaults().extend([mousePositionControl]), // add mouse position
+  layers: [raster,osm_lines,osm_points], // layers stacked by order (arg0 under arg1 under arg2 etc)
   target: 'map',
-  view: view,
+  view: map_view,
 });
 
+
+///////////////////////////////////////// FUNCTIONS ////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// display feature info on click.
 map.on('singleclick', function (evt) {
   document.getElementById('info').innerHTML = '';
-  var viewResolution = /** @type {number} */ (view.getResolution());
-  var url = wmsSource.getFeatureInfoUrl(
-    evt.coordinate,
-    viewResolution,
-    'EPSG:3857',
+  var viewResolution = map_view.getResolution();
+  var url = imgsrc_points.getFeatureInfoUrl( //  here i have to choose 1 source? (points or lines for now)
+    evt.coordinate, viewResolution, 'EPSG:3857',
     {'INFO_FORMAT': 'text/html'}
   );
   if (url) {
-    fetch(url)
+    console.log("fetching..");
+
+    fetch(url) 
       .then(function (response) { return response.text(); })
       .then(function (html) {
-        document.getElementById('info').innerHTML = html;
+        document.getElementById('info').innerHTML = ('Info :'+html);
+        console.log('html:',html);
       });
+    //
   }
 });
 
-map.on('pointermove', function (evt) {
-  if (evt.dragging) {
-    return;
-  }
-  var pixel = map.getEventPixel(evt.originalEvent);
-  var hit = map.forEachLayerAtPixel(pixel, function () {
-    return true;
-  });
-  map.getTargetElement().style.cursor = hit ? 'pointer' : '';
-});
+
